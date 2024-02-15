559947aec80af1f7b8d34698750345c12c0f8850
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2024-02-15 18:07:00 -0300

    feat: using namespaces and using

--- Screen-Sound-2.0/Program.cs
+++ Screen-Sound-2.0/Program.cs
@@ -1,4 +1,9 @@
-﻿Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
+﻿using ScreenSound.Models;
+
+Banda ira = new("Ira!");
+Banda beatles = new("Beatles")
+
+Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
 bandasRegistradas.Add("Linkin Park", new List<int> { 10, 8, 6 });
 bandasRegistradas.Add("The Beatles", new List<int>());
 
