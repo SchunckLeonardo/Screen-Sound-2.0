355853c0436acb528096db6d2048e418ee7ffbd8
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2024-02-15 19:07:45 -0300

    feat: Creating Menu Classes

---
+++ Screen-Sound-2.0/Models/Menu.cs
@@ -0,0 +1,13 @@
+﻿namespace ScreenSound.Models;
+
+internal class Menu
+{
+    public static void ExibirTituloDaOpcao(string titulo)
+    {
+        int quantidadeDeLetras = titulo.Length;
+        string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
+        Console.WriteLine(asteriscos);
+        Console.WriteLine(titulo);
+        Console.WriteLine(asteriscos + "\n");
+    }
+}
